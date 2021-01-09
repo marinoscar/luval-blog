0f4ad43d1bdcbfefe16cc74bedb20d3484b7df38
marinoscar <oscar@marin.cr>
2021-01-09 09:58:46 -0600

    added web config options

---
+++ code/Luval.Blog.Web/BlogConfigureOptions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.Blog.Web
+{
+    public class BlogConfigureOptions : IPostConfigureOptions<StaticFileOptions>
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public BlogConfigureOptions(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public void PostConfigure(string name, StaticFileOptions options)
+        {
+
+            options.ContentTypeProvider ??= new FileExtensionContentTypeProvider();
+
+            if (options.FileProvider == null && _environment.WebRootFileProvider == null)
+            {
+                throw new InvalidOperationException("Missing FileProvider.");
+            }
+
+            options.FileProvider = options.FileProvider ?? _environment.WebRootFileProvider;
+
+
+            // Add our provider
+            var filesProvider = new ManifestEmbeddedFileProvider(GetType().Assembly, "resources");
+            options.FileProvider = new CompositeFileProvider(options.FileProvider, filesProvider);
+        }
+    }
+}
---
+++ code/Luval.Blog.Web/BlogServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.Blog.Web
+{
+    public static class BlogServiceCollectionExtensions
+    {
+        public static void AddBlog(this IServiceCollection services)
+        {
+        }
+    }
+}
