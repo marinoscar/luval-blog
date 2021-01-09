b4f2f568d38d8e25ff14eff8f915bcd931e35af7
marinoscar <oscar@marin.cr>
2021-01-09 17:20:32 -0600

    updated the icon

--- code/Luval.Blog.Web/resources/lib/luval.blog/blog.js
+++ code/Luval.Blog.Web/resources/lib/luval.blog/blog.js
@@ -4,6 +4,19 @@
 
     },
     initEditor: function () {
-        blog.editor = new SimpleMDE({ element: document.getElementById("MarkDownEditor") });
+        blog.editor = new SimpleMDE({
+            element: document.getElementById("MarkDownEditor"),
+			toolbar: [
+				"undo", "redo", "bold", "italic", "strikethrough", "heading", "|", "quote", "unordered-list", "ordered-list", "table", "horizontal-rule", "|",
+				"link", "image", {
+					name: "custom",
+					action: function openMedia(editor) {
+						window.open('/Media', '_blank');
+					},
+					className: "fa fa-folder-open",
+					title: "add media",
+				}, "code", "clean-block", "|", "preview", "side-by-side", "fullscreen", "guide"
+			],
+        });
     }
 }
